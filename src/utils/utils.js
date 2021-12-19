648a36a50faddf727c9d3619d7ddc50c9ed2e97a
Park Yeonha <49088833+YeonhaPark@users.noreply.github.com>
2021-12-18 18:38:14 -0600

    delete: 사용되지 않는 유틸 함수 제거

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,3 @@
 export const $ = (property) => {
   return document.querySelector(property);
 } 
-
-export const generateId = function* (givenId) {
-  let id = givenId;
-  while (true) {
-    id++;
-    yield id;
-  }
-}
